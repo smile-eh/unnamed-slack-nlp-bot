373ea5d0531e1473aa41def23a630a973f8d837f
Smile <matthew.sta.clements@gmail.com>
2019-09-23 17:52:29 -0400

    add message tests

---
+++ tests/test_message.py
@@ -0,0 +1,23 @@
+import pytest
+import src.message as message
+
+class TestMessage:
+    def test_create_message(self):
+        message_obj = message.Message()
+        assert message_obj.channel == ""
+        assert message_obj.text == ("Category\n"
+                     ">%s\n"
+                     "Summarized article sent to Google NLP\n"
+                     ">>>%s")
+
+    def test_update_message(self):
+        message_obj = message.Message()
+        assert message_obj.text == ("Category\n"
+                     ">%s\n"
+                     "Summarized article sent to Google NLP\n"
+                     ">>>%s")
+        message_obj.update_text_categorized("test/category", "Test Original Text")
+        assert message_obj.text == ("Category\n"
+                     ">test/category\n"
+                     "Summarized article sent to Google NLP\n"
+                     ">>>Test Original Text")
